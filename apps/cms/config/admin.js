4b8624616681c2714b321b57f956694687ea03aa
Mehmet <onuur.s@gmail.com>
2022-08-24 22:37:06 +0200

    feat(cms): pages content-type

--- apps/cms/config/admin.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env("ADMIN_JWT_SECRET", "super-secret-key"),
-  },
-  apiToken: {
-    salt: env("API_TOKEN_SALT", "super-secret-salt"),
-  },
-});
