053ee085a6b823cbae3b2c5f17aa6b8c188582ef
Mehmet <onuur.s@gmail.com>
2022-09-15 15:27:29 +0200

    feat: add docs to docker

--- apps/cms/config/server.ts
+++ apps/cms/config/server.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default ({ env }: any) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 3000),
+  port: env.int('PORT', 9001),
   app: {
     keys: env.array('APP_KEYS'),
   },
