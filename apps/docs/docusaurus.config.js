5f6568b8bf2637d3027e29bf6a9c9f480879b3dc
Mehmet <onuur.s@gmail.com>
2022-11-21 13:22:13 +0100

    fix: broken links for docs

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -36,14 +36,15 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
+
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/MinBZK/regels.overheid.nl/tree/develop/docs',
+          editUrl: 'https://github.com/MinBZK/regels.overheid.nl/tree/develop/apps/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/MinBZK/regels.overheid.nl/tree/develop/docs',
+          editUrl: 'https://github.com/MinBZK/regels.overheid.nl/tree/develop/apps/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
