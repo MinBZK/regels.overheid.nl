975fe76b72b72c2038497602dac60450f0e6aef2
Mehmet <onuur.s@gmail.com>
2022-11-25 01:09:59 +0100

    feat: methods page

--- apps/web/src/containers/layout.tsx
+++ apps/web/src/containers/layout.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@mui/material';
 import { Navbar } from '../components/navbar';
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <>
       <Navbar />
-      {children}
+      <Box py={9} display="grid" gridTemplateColumns="1fr [main-start] repeat(5, 1fr) [main-end] 1fr">
+        <Box gridArea="main">{children}</Box>
+      </Box>
     </>
   );
 };
---
+++ apps/web/src/pages/methoden.tsx
@@ -0,0 +1,51 @@
+import { Box, Card, CardContent, CardMedia, Stack, Typography } from '@mui/material';
+import { GetServerSideProps } from 'next';
+import { getMethods, GetMethodsResponse } from '../services/strapi/get-methods';
+
+interface Props {
+  methods: GetMethodsResponse;
+}
+
+const Methoden: React.FC<Props> = ({ methods }) => {
+  return (
+    <Stack spacing={2}>
+      <Typography variant="h3">Methoden</Typography>
+      <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" columnGap={4} alignItems="flex-start">
+        {methods.data.map((method) => {
+          const mediaURL = new URL(
+            `/cms${method.attributes.Visual?.data.attributes.formats.medium.url}`,
+            process.env.NEXT_PUBLIC_CMS_API
+          );
+          return (
+            <Card key={method.id}>
+              {/* <CardMedia
+                component="img"
+                height="300"
+                image={mediaURL.toString()}
+                alt={method.attributes.Visual?.data.attributes.formats.medium.name}
+              /> */}
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {method.attributes.Title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {method.attributes.Description}
+                </Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Box>
+    </Stack>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  return {
+    props: {
+      methods: await getMethods({ includeVisual: true }),
+    },
+  };
+};
+
+export default Methoden;
