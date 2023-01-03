a35623d8fb41ce708e8f875e4e1e574a486d7fd0
Mehmet <onuur.s@gmail.com>
2023-01-03 08:47:14 +0100

    feat: footer component

--- apps/web/src/containers/layout.tsx
+++ apps/web/src/containers/layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from '@mui/material';
 import { Container } from '../components/container';
+import { Footer } from '../components/footer';
 import { Navbar } from '../components/navbar';
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <Stack>
+    <Stack minHeight="100vh" direction="column">
       <Navbar />
-      <Container>{children}</Container>
+      <Container pb={5}>{children}</Container>
+      <Footer mt="auto" />
     </Stack>
   );
 };
