b1a9a2910def4c07b23eb67b8911d8f079fab0c7
datafluisteraar <steven.gort@ictu.nl>
2023-01-04 16:51:09 +0100

    feat: add favicon

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <title>Regelregister van de Nederlandse Overheid</title>
+        <link rel="icon" type="image/png" href="/public/favicon.ico"></link>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
