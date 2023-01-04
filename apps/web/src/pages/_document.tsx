464c1620389dd300e7a4372a20bc334e376f19fa
datafluisteraar <steven.gort@ictu.nl>
2023-01-04 16:25:00 +0100

    feat: add responsive viewport meta tag

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
