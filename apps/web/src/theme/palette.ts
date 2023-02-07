10288e6bf9b0f5b99e56a9f8c8014fb2f40f8681
Mehmet <onuur.s@gmail.com>
2023-02-08 00:28:46 +0100

    feat: update color palette

--- apps/web/src/theme/palette.ts
+++ apps/web/src/theme/palette.ts
@@ -1,16 +1,33 @@
 import { PaletteOptions } from '@mui/material';
 
 export const palette: PaletteOptions = {
-  /**
-   * Rijkshuisstijl palet
-   *
-   * Hemelblauw #007bc7 | rgb(0, 123, 199)
-   * Tint 1 #b3d7ee | rgb(179, 215, 238)
-   * Tint 2 #d9ebf7 | rgb(217, 235, 247)
-   */
   primary: {
-    main: '#007bc7',
-    light: '#b3d7ee',
-    lighter: '#d9ebf7',
+    dark: '#154270',
+    main: '#007BC0',
+    light: '#B2D7EA',
+    lighter: '#E5F1F9',
+  },
+  info: {
+    dark: '#007BC0',
+    main: '#B2D7EA',
+    light: '#E5F1F9',
+  },
+  warning: {
+    dark: '#E17000',
+    main: '#E78D33',
+    light: '#EDA966',
+    lighter: '#FCF0E5',
+  },
+  error: {
+    dark: '#C70039',
+    main: '#D23361',
+    light: '#E06694',
+    lighter: '#F9E6EB',
+  },
+  success: {
+    dark: '#00855F',
+    main: '#339D7F',
+    light: '#66B69F',
+    lighter: '#E6F3EF',
   },
 };
