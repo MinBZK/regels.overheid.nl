ff3c752c79253c66f00eabafcae2901867e716b0
Mehmet <onuur.s@gmail.com>
2023-02-09 22:07:01 +0100

    feat: add storybook

---
+++ apps/web/.storybook/preview.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../src/theme';
+
+export const parameters = {
+  backgrounds: {
+    default: 'light',
+  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
