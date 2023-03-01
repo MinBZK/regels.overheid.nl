565b70afacd9ec898490b6a0b53bb2bd1eff13b3
Mehmet <onuur.s@gmail.com>
2023-03-01 16:55:03 +0100

    fix: database connection string not being an environment var

--- apps/web/src/helpers/strapi-database.ts
+++ apps/web/src/helpers/strapi-database.ts
@@ -1,5 +1,5 @@
 import { Pool } from 'pg';
 
 export const strapiDatabase = new Pool({
-  connectionString: 'postgresql://postgres:postgres@postgres:5432/strapi',
+  connectionString: process.env.CMS_DATABASE_URL,
 });
