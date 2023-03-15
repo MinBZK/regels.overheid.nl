ea2031dc6bd5454c8cebc2eeb756a9c550883346
Mehmet <onuur.s@gmail.com>
2023-03-15 23:32:35 +0100

    feat: version number in footer

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,30 +1,42 @@
+import { ApplicationMetadataContext } from '@/components/application-metadata-provider';
 import { PagesContext } from '@/components/pages-provider';
 import { Layout } from '@/containers/layout';
-import { getPages } from '@/services/cms/get-pages';
+import { ApplicationMetadata, getApplicationMetadata } from '@/services/cms/get-application-metadata';
+import { getPages, Page } from '@/services/cms/get-pages';
 import { theme } from '@/theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { AppProps } from 'next/app';
 import '@total-typescript/ts-reset';
+import { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  const { pages, ...componentProps } = pageProps;
+interface PageProps {
+  pages: Page[];
+  applicationMetadata?: ApplicationMetadata;
+}
+
+const App = ({ Component, pageProps }: Omit<AppProps, 'pageProps'> & { pageProps: PageProps }) => {
+  const { pages, applicationMetadata, ...componentProps } = pageProps;
 
   return (
     <PagesContext.Provider value={{ pages }}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Layout>
-          <Component {...componentProps}></Component>
-        </Layout>
-      </ThemeProvider>
+      <ApplicationMetadataContext.Provider value={{ data: applicationMetadata?.attributes }}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout>
+            <Component {...componentProps}></Component>
+          </Layout>
+        </ThemeProvider>
+      </ApplicationMetadataContext.Provider>
     </PagesContext.Provider>
   );
 };
 
-App.getInitialProps = async () => {
+App.getInitialProps = async (): Promise<{ pageProps: PageProps }> => {
   const pages = await getPages().then((res) => res.data);
+  const applicationMetadata = await getApplicationMetadata()
+    .then((res) => res.data)
+    .catch(() => undefined);
 
-  return { pageProps: { pages } };
+  return { pageProps: { pages, applicationMetadata } };
 };
 
 export default App;
