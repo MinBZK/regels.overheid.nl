1c8dfdf7ae6eac1bbde91677f684c7c1f86239ac
Mehmet <onuur.s@gmail.com>
2023-03-24 01:35:45 +0100

    feat: RightAngleRulerIcon

--- apps/web/src/theme/icons/right-angle-ruler-icon.tsx
+++ apps/web/src/theme/icons/right-angle-ruler-icon.tsx
@@ -1,1 +1,62 @@
+import { SvgIcon, SvgIconProps } from '@mui/material';
+
 export const RightAngleRulerIcon: React.FC<SvgIconProps> = (props) => (
+  <SvgIcon viewBox="0 0 108 107" {...props}>
+    <path
+      d="M22.8203 18.127H85.2369C86.4194 18.127 87.5534 18.5967 88.3895 19.4328C89.2256 20.2689 89.6953 21.4029 89.6953 22.5853V44.877C89.6953 46.0594 89.2256 47.1934 88.3895 48.0295C87.5534 48.8656 86.4194 49.3353 85.2369 49.3353H54.0286C52.8462 49.3353 51.7122 49.805 50.8761 50.6411C50.04 51.4772 49.5703 52.6112 49.5703 53.7936V85.0019C49.5703 86.1844 49.1006 87.3184 48.2645 88.1545C47.4284 88.9906 46.2944 89.4603 45.1119 89.4603H22.8203C21.6378 89.4603 20.5039 88.9906 19.6678 88.1545C18.8317 87.3184 18.3619 86.1844 18.3619 85.0019V22.5853C18.3619 21.4029 18.8317 20.2689 19.6678 19.4328C20.5039 18.5967 21.6378 18.127 22.8203 18.127Z"
+      stroke={props.color || '#000'}
+      fill="none"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18.3619 35.96H27.2786"
+      stroke={props.color || '#000'}
+      fill="none"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18.3619 53.7935H31.7369"
+      stroke={props.color || '#000'}
+      fill="none"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18.3619 71.627H27.2786"
+      stroke={props.color || '#000'}
+      fill="none"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M36.1949 18.127V27.0436"
+      stroke={props.color || '#000'}
+      fill="none"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M54.0284 18.127V31.502"
+      stroke={props.color || '#000'}
+      fill="none"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M71.8619 18.127V27.0436"
+      stroke={props.color || '#000'}
+      fill="none"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </SvgIcon>
+);
