b811b7a2363eabd7894e5aae57ecbab7cdeda5bc
Mehmet <onuur.s@gmail.com>
2023-03-28 17:33:29 +0200

    fix: changed html-attributes to react attributes

--- apps/web/src/theme/icons/arch-icon.tsx
+++ apps/web/src/theme/icons/arch-icon.tsx
@@ -2,20 +2,20 @@ import { SvgIcon, SvgIconProps } from '@mui/material';
 
 export const ArchIcon: React.FC<SvgIconProps> = (props) => (
   <SvgIcon viewBox="0 0 108 107" {...props}>
-    <path stroke-width="4" stroke-linecap="round" stroke-linejoin="round" d="M13.9081 93.625H94.3562" />
+    <path strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" d="M13.9081 93.625H94.3562" />
     <path
       d="M18.3773 93.625V26.75C18.3773 24.3851 19.3191 22.1171 20.9954 20.4449C22.6717 18.7727 24.9453 17.8333 27.316 17.8333H80.9481C83.3188 17.8333 85.5924 18.7727 87.2687 20.4449C88.945 22.1171 89.8868 24.3851 89.8868 26.75V93.625"
       fill="none"
-      stroke-width="4"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M40.7241 93.625V57.9583C40.7241 54.411 42.1367 51.0091 44.6512 48.5008C47.1657 45.9925 50.5761 44.5833 54.1321 44.5833C57.6882 44.5833 61.0986 45.9925 63.6131 48.5008C66.1275 51.0091 67.5402 54.411 67.5402 57.9583V93.625"
       fill="none"
-      stroke-width="4"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </SvgIcon>
 );
