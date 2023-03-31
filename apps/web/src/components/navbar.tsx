8be6ac2b49c4e3fb6abda62d6afd6ba14f59b808
Mehmet <onuur.s@gmail.com>
2023-03-31 03:14:07 +0200

    refactor(web): share common logic between mobile and desktop nav

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,16 +1,31 @@
+import { Page } from '@/services/cms/get-pages';
 import { Box } from '@mui/material';
+import { useRouter } from 'next/router';
 
 import { NavbarDesktop } from './navbar/navbar-desktop';
 import { NavbarMobile } from './navbar/navbar-mobile';
+import { usePagesContext } from './pages-provider';
+
+export interface SubComponentProps {
+  isPageActive: (page: Page) => boolean;
+}
 
 export const Navbar: React.FC = () => {
+  const router = useRouter();
+  const { pages } = usePagesContext();
+
+  const isPageActive = (page: Page) => {
+    if (page.attributes.slug === 'home') return router.pathname === '/';
+
+    return router.asPath === `/${page.attributes.slug}`;
+  };
   return (
     <>
       <Box display={['block', 'none']}>
-        <NavbarMobile />
+        <NavbarMobile isPageActive={isPageActive} />
       </Box>
       <Box display={['none', 'block']}>
-        <NavbarDesktop />
+        <NavbarDesktop isPageActive={isPageActive} />
       </Box>
     </>
   );
