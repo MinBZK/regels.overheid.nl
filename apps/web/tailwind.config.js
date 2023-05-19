56477b477077f93578355a1924aec273a23977a7
Mehmet <onuur.s@gmail.com>
2023-05-19 11:57:08 +0200

    chore: update tailwind theme

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#212427',
+        grey: {
+          dark: '#4D5052',
+          main: '#7A7C7D',
+          light: '#CECECE',
+          lighter: '#EFEFEF',
+        },
+        primary: {
+          dark: '#154270',
+          main: '#007BC0',
+          light: '#B2D7EA',
+          lighter: '#E5F1F9',
+        },
+      },
+    },
   },
   plugins: [],
 };
