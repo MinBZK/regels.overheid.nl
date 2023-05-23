1c5be567013a902973d811080f8ddc733c71739b
Mehmet <onuur.s@gmail.com>
2023-05-24 01:22:10 +0200

    feat: Tailwind typography component

---
+++ apps/web/src/components/typography.tsx
@@ -0,0 +1,22 @@
+import { clsx } from 'clsx';
+
+type TypographyVariantTypes = 'page-title';
+
+type TypographyVariant = [component: React.ElementType, classNames: string];
+
+const typographyVariantMapping: Record<TypographyVariantTypes, TypographyVariant> = {
+  'page-title': ['h1', 'text-5xl mb-6'],
+};
+
+interface Props extends React.PropsWithChildren {
+  variant: TypographyVariantTypes;
+  component?: React.ElementType;
+  className?: string;
+}
+
+export const Typography: React.FC<Props> = ({ children, component, className, variant }) => {
+  const [defaultComponent, defaultClassName] = typographyVariantMapping[variant];
+  const Component = component || defaultComponent;
+
+  return <Component className={`${defaultClassName} ${clsx(className)}`}>{children}</Component>;
+};
