9d27d20b4dae76c8ad7bffffea1a6827d23307d4
Mehmet <onuur.s@gmail.com>
2023-05-25 22:16:28 +0200

    refactor: Tailwind roadmap page

--- apps/web/src/stories/pages/roadmap.stories.tsx
+++ apps/web/src/stories/pages/roadmap.stories.tsx
@@ -1,22 +1,9 @@
-import React from 'react';
 import { Meta } from '@storybook/react';
-import { Container } from '../../components/container';
 import Roadmap from '../../pages/roadmap';
-import { getMethodsMock } from '../../services/cms/mocks/get-methods-mock';
 
 const meta: Meta<typeof Roadmap> = {
   title: 'Pages/Roadmap',
   component: Roadmap,
-  args: {
-    methods: getMethodsMock,
-  },
-  decorators: [
-    (Story) => (
-      <Container overflow="hidden">
-        <Story />
-      </Container>
-    ),
-  ],
 };
 
 export default meta;
