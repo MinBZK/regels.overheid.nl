9aa862b1f12dc0f80ca7578b24c43e90848aced0
Mehmet <onuur.s@gmail.com>
2023-06-01 01:39:07 +0200

    chore: remove mui

--- apps/web/src/components/page-title.tsx
+++
@@ -1,7 +0,0 @@
-import { Typography, TypographyProps } from '@mui/material';
-
-export const PageTitle: React.FC<TypographyProps<'h1'>> = ({ children, ...props }) => (
-  <Typography component="h1" fontSize={36} fontWeight="bold" mb={6} {...props}>
-    {children}
-  </Typography>
-);
