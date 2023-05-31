7c1756649848fab45ba05a16976b997fff5e9cea
Mehmet <onuur.s@gmail.com>
2023-06-01 01:38:40 +0200

    fix: makeURL fallback to NEXT_PUBLIC_CMS_ROOT_URL for client side calls

--- apps/web/src/services/cms/make-url.ts
+++ apps/web/src/services/cms/make-url.ts
@@ -1,1 +1,9 @@
-export const makeURL = (path: string) => new URL(path, process.env.CMS_API_URL);
+const getCmsUrl = () => {
+  if (process.env.CMS_API_URL) return process.env.CMS_API_URL;
+
+  if (process.env.NEXT_PUBLIC_CMS_ROOT_URL) return `${process.env.NEXT_PUBLIC_CMS_ROOT_URL}/api`;
+
+  throw new Error('CMS_API_URL or NEXT_PUBLIC_CMS_ROOT_URL must be defined');
+};
+
+export const makeURL = (path: string) => new URL(path, getCmsUrl());
