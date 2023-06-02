d020b66d6543ef9cc772a510b03ed8cb3f0cdd0c
Mehmet <onuur.s@gmail.com>
2023-06-02 12:24:43 +0200

    fix: disable cache on get-page-by-slug

--- apps/web/src/services/cms/common-fetch.ts
+++ apps/web/src/services/cms/common-fetch.ts
@@ -1,9 +1,9 @@
 import { makeURL } from './make-url';
 
-export const commonFetch = async <T>(path: string) => {
+export const commonFetch = async <T>(path: string, init?: RequestInit) => {
   const url = makeURL(path);
 
-  const response = await fetch(url.toString());
+  const response = await fetch(url.toString(), init);
 
   const json = await response.json();
 
