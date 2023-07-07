710c5fa10c6f795376a0a65f1de8c8dd15660c3f
Mehmet <onuur.s@gmail.com>
2023-07-07 21:44:07 +0200

    feat: change button to OverrideAbleComponentFunctionComponent

--- apps/web/src/components/button.tsx
+++ apps/web/src/components/button.tsx
@@ -1,11 +1,14 @@
+import { OverrideAbleComponentFunctionComponent } from '@/helpers/override-able-component-function-component';
+import { clsx } from 'clsx';
+
 type Variant = 'contained' | 'text';
 
 interface Props extends React.PropsWithChildren {
-  href?: string;
   endIcon?: JSX.Element;
   startIcon?: JSX.Element;
-  component?: React.ElementType;
   variant?: Variant;
+  className?: string;
+  disabled?: boolean;
 }
 
 const buttonVariantMap: Record<Variant, string> = {
@@ -13,18 +16,31 @@ const buttonVariantMap: Record<Variant, string> = {
   contained: 'bg-primary-main text-white hover:bg-primary-dark',
 };
 
-export const Button: React.FC<Props> = ({
+const disabledButtonVariantMap: Record<Variant, string> = {
+  contained: 'bg-grey-light text-grey-dark',
+  text: 'bg-transparent text-grey-main',
+};
+
+export const Button: OverrideAbleComponentFunctionComponent<'button', Props> = ({
   children,
   component: Component = 'button',
   endIcon,
-  href,
   startIcon,
   variant = 'contained',
+  className,
+  disabled,
+  ...htmlProps
 }) => {
   return (
     <Component
-      href={href}
-      className={`h-10 px-4 flex items-center rounded-lg text-base font-bold ${buttonVariantMap[variant]} `}
+      className={clsx(
+        'h-10 px-4 flex items-center rounded-lg text-base font-bold',
+        !disabled && buttonVariantMap[variant],
+        disabled && disabledButtonVariantMap[variant],
+        className
+      )}
+      {...htmlProps}
+      onClick={disabled === false ? htmlProps?.onClick : undefined}
     >
       {startIcon && <span className="mr-3">{startIcon}</span>}
       {children}
