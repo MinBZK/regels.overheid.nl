e9835fcf12c29e9e6892ee10d2e9968550f0e892
Mehmet <onuur.s@gmail.com>
2024-01-29 22:43:41 +0100

    fix: add path.resolve() + process.cwd() on  publicationPath for better file path handling

--- apps/web/src/app/(common-page)/publicaties/[...slug]/route.ts
+++ apps/web/src/app/(common-page)/publicaties/[...slug]/route.ts
@@ -1,11 +1,12 @@
 import fs from 'fs/promises';
+import path from 'path';
 
 export async function GET(request: Request) {
   const enhancedUrl = new URL(request.url);
 
   const [, , publication, version = 'latest'] = enhancedUrl.pathname.split('/');
 
-  const publicationPath = `public/public/publications/${publication}/${version}.html`;
+  const publicationPath = path.resolve(process.cwd(), `public/public/publications/${publication}/${version}.html`);
 
   const publicationExists = await fs
     .access(publicationPath)
