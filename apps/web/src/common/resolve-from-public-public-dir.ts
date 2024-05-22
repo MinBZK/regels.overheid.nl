cba29f8d100c99a51b31b9b0c9376c75ffe714fb
Mehmet <onuur.s@gmail.com>
2024-05-22 23:40:17 +0200

    refactor: use process.cwd

--- apps/web/src/common/resolve-from-public-public-dir.ts
+++ apps/web/src/common/resolve-from-public-public-dir.ts
@@ -4,5 +4,5 @@ import path from 'path';
  * the "public public dir" is a mounted directory which is handy for files managed in production
  */
 export function resolveFromPublicPublicDir(...paths: string[]) {
-  return path.resolve(__dirname, '../../../../../../public/public/', ...paths);
+  return path.resolve(__dirname, process.cwd(), 'public/public/', ...paths);
 }
