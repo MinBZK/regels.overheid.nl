49d8fc1a32769bbe198b4819782c712ca4f5922b
Mehmet Onur Sagir <onur@Mehmets-MacBook-Pro.local>
2025-11-16 23:22:41 +0100

    feat: implement azure blob storage for uploads

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default ({ env }) => {
+  const plugins: Record<string, any> = {};
+
+  if (env('AZURE_ACCOUNT_KEY'))
+    plugins.upload = {
+      config: {
+        provider: 'strapi-provider-upload-azure-sa',
+        providerOptions: {
+          account: env('AZURE_ACCOUNT_NAME'),
+          accountKey: env('AZURE_ACCOUNT_KEY'),
+          sasToken: env('AZURE_SAS_TOKEN'),
+          containerName: env('AZURE_CONTAINER_NAME'),
+          defaultPath: env('AZURE_DEFAULT_PATH', 'uploads'),
+          cdnBaseURL: env('AZURE_CDN_BASE_URL'),
+          serviceBaseURL: env('AZURE_SERVICE_BASE_URL'),
+          defaultCacheControl: env('AZURE_DEFAULT_CACHE_CONTROL'),
+          removeCN: env('AZURE_REMOVE_CN'),
+        },
+      },
+    };
+
+  return plugins;
+};
