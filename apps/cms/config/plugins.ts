2e49e020d113ca5cbb2560d7f6e8b6d587baf11d
Mehmet Onur Sagir <onur@Mehmets-MacBook-Pro.local>
2025-11-18 12:37:07 +0100

    feat: enable graphql

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default () => ({
+  graphql: {
+    shadowCRUD: true,
+    endpoint: '/graphql',
+    subscriptions: false,
+    maxLimit: -1,
+    apolloServer: {},
+    v4CompatibilityMode: process.env.STRAPI_GRAPHQL_V4_COMPATIBILITY_MODE ?? false,
+  },
+});
