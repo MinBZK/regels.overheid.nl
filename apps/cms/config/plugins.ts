84cd6fd1fe8926b67df9a78c0a68e87abcc79d2c
Mehmet Onur Sagir <onur@Mehmets-MacBook-Pro.local>
2025-11-20 14:54:26 +0100

    feat: added azure upload provider

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,10 +1,32 @@
-export default () => ({
-  graphql: {
-    shadowCRUD: true,
-    endpoint: '/graphql',
-    subscriptions: false,
-    maxLimit: -1,
-    apolloServer: {},
-    v4CompatibilityMode: process.env.STRAPI_GRAPHQL_V4_COMPATIBILITY_MODE ?? false,
-  },
-});
+export default ({ env }) => {
+  const plugins: Record<string, any> = {
+    graphql: {
+      shadowCRUD: true,
+      endpoint: '/graphql',
+      subscriptions: false,
+      maxLimit: Infinity,
+      apolloServer: {},
+      v4CompatibilityMode: process.env.STRAPI_GRAPHQL_V4_COMPATIBILITY_MODE ?? false,
+    },
+  };
+
+  if (env('AZURE_ACCOUNT_KEY'))
+    plugins.upload = {
+      config: {
+        provider: 'strapi-provider-upload-azure-sa',
+        providerOptions: {
+          account: env('AZURE_ACCOUNT_NAME'),
+          accountKey: env('AZURE_ACCOUNT_KEY'),
+          sasToken: env('AZURE_SAS_TOKEN'),
+          containerName: env('AZURE_CONTAINER_NAME'),
+          defaultPath: env('AZURE_DEFAULT_PATH', 'uploads'),
+          cdnBaseURL: env('AZURE_CDN_BASE_URL'),
+          serviceBaseURL: env('AZURE_SERVICE_BASE_URL'),
+          defaultCacheControl: env('AZURE_DEFAULT_CACHE_CONTROL'),
+          removeCN: env('AZURE_REMOVE_CN'),
+        },
+      },
+    };
+
+  return plugins;
+};
